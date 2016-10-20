007db62dceb2ddea20eb4ac1c4e6f48d76e52e23
Markvanlierop <mark-vl@live.nl>
2016-10-20 13:33:57 +0200

    queries aangemaakt

--- EyeCT4Events-WF/Classes/Interfaces/IGebruikerAdministratie.cs
+++ EyeCT4Events-WF/Classes/Interfaces/IGebruikerAdministratie.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EyeCT4Events_WF.Classes.Gebruikers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace EyeCT4Events_WF.Interfaces
 {
     public interface IGebruikerAdministratie
     {
-        void AlleBezoekersOpvragen();
+        List<Bezoeker> LijstBezoekers();
     }
 }
