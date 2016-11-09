4ee166b7b0a689b43b5c3c326bcac7e73f0535fe
i268730 <268730@student.fontys.nl>
2016-11-09 17:04:55 +0100

    ffv

---
+++ EyeCT4Events-WF/Forms/SocialMediaSharing/FormAanwezigeBezoekers.cs
@@ -0,0 +1,119 @@
+﻿using EyeCT4Events_WF.Classes;
+using EyeCT4Events_WF.Classes.Repositories;
+using EyeCT4Events_WF.Exceptions;
+using EyeCT4Events_WF.Forms.BeheerSysteem;
+using EyeCT4Events_WF.Persistencies;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EyeCT4Events_WF
+{
+    public partial class FormAanwezigeBezoekers : Form
+    {
+        private RepositoryGebruiker rg;
+        private RepositoryKampeerPlaatsen rkp;
+        private List<Gebruiker> bezoekerLijst;
+        private List<Kampeerplaats> kampeerplaatsLijst;
+
+        public FormAanwezigeBezoekers()
+        {
+            InitializeComponent();
+            rg = new RepositoryGebruiker(new MSSQL_Server());
+            rkp = new RepositoryKampeerPlaatsen(new MSSQL_Server());
+            bezoekerLijst = new List<Gebruiker>();
+
+            lvAanwezigeBezoekers.View = View.Details;
+            lvAanwezigeBezoekers.FullRowSelect = true;
+            lvAanwezigeBezoekers.Columns.Add("Bezoeker");
+            lvAanwezigeBezoekers.Columns.Add("Aanwezig");
+            lvAanwezigeBezoekers.Columns.Add("Kampeerplaats");
+        }
+        private void AanwezigeBezoekerslijstVullen()
+        {
+            lvAanwezigeBezoekers.Items.Clear();
+            try
+            {
+                bezoekerLijst = rg.LijstAanwezigeBezoekers();
+                kampeerplaatsLijst = rkp.AlleKampeerplaatsenOpvragen();
+            }
+            catch (FoutBijUitvoerenQueryException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            foreach (Gebruiker g in bezoekerLijst)
+            {
+                ListViewItem lvi = new ListViewItem(g.ToString());
+                lvi.SubItems.Add(g.Aanwezig ? "Ja" : "Nee");
+                foreach (Kampeerplaats k in kampeerplaatsLijst)
+                {
+                    if (k.CheckOfBezoekerOpKampeerplaatsIs(g))
+                    {
+                        lvi.SubItems.Add(k.ID.ToString());
+                    }
+                }
+                lvAanwezigeBezoekers.Items.Add(lvi);
+            }
+            lvAanwezigeBezoekers.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.ColumnContent);
+            lvAanwezigeBezoekers.AutoResizeColumn(2, ColumnHeaderAutoResizeStyle.HeaderSize);
+
+        }
+        private void AanwezigBezoekers_Load(object sender, EventArgs e)
+        {
+            AanwezigeBezoekerslijstVullen();
+        }
+
+        private void lbSluiten_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void txtBezoekerAanmelden_TextChanged(object sender, EventArgs e)
+        {
+            if (txtBezoekerAanmelden.Text != "")
+            {
+                try
+                {
+                    rg.ZetGebruikerOpAanwezig(Convert.ToInt32(txtBezoekerAanmelden.Text));
+                    txtBezoekerAanmelden.Text = "";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                AanwezigeBezoekerslijstVullen();
+            }
+        }
+
+        private void txtBezoekerAfmelden_TextChanged(object sender, EventArgs e)
+        {
+            if (txtBezoekerAfmelden.Text != "")
+            {
+                try
+                {
+                    rg.ZetGebruikerOpAfwezig(Convert.ToInt32(txtBezoekerAfmelden.Text));
+                    txtBezoekerAfmelden.Text = "";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                AanwezigeBezoekerslijstVullen();
+            }
+        }
+
+        private void btnBetalingsgegevens_Click(object sender, EventArgs e)
+        {
+            FormBetalingsGegevens fbg = new FormBetalingsGegevens();
+            fbg.ShowDialog();
+        }
+    }
+}
---
+++ EyeCT4Events-WF/Forms/SocialMediaSharing/FormAanwezigeBezoekers.designer.cs
@@ -0,0 +1,158 @@
+﻿namespace EyeCT4Events_WF
+{
+    partial class FormAanwezigeBezoekers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAanwezigeBezoekers = new System.Windows.Forms.Label();
+            this.lbSluiten = new System.Windows.Forms.Button();
+            this.lvAanwezigeBezoekers = new System.Windows.Forms.ListView();
+            this.txtBezoekerAanmelden = new System.Windows.Forms.TextBox();
+            this.lblBezoekerAanmelden = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBezoekerAfmelden = new System.Windows.Forms.TextBox();
+            this.btnBetalingsgegevens = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblAanwezigeBezoekers
+            // 
+            this.lblAanwezigeBezoekers.AutoSize = true;
+            this.lblAanwezigeBezoekers.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblAanwezigeBezoekers.Location = new System.Drawing.Point(12, 9);
+            this.lblAanwezigeBezoekers.Name = "lblAanwezigeBezoekers";
+            this.lblAanwezigeBezoekers.Size = new System.Drawing.Size(225, 26);
+            this.lblAanwezigeBezoekers.TabIndex = 9;
+            this.lblAanwezigeBezoekers.Text = "Aanwezige bezoekers";
+            this.lblAanwezigeBezoekers.UseMnemonic = false;
+            // 
+            // lbSluiten
+            // 
+            this.lbSluiten.Location = new System.Drawing.Point(17, 372);
+            this.lbSluiten.Name = "lbSluiten";
+            this.lbSluiten.Size = new System.Drawing.Size(86, 27);
+            this.lbSluiten.TabIndex = 10;
+            this.lbSluiten.Text = "Sluiten";
+            this.lbSluiten.UseVisualStyleBackColor = true;
+            this.lbSluiten.Click += new System.EventHandler(this.lbSluiten_Click);
+            // 
+            // lvAanwezigeBezoekers
+            // 
+            this.lvAanwezigeBezoekers.Location = new System.Drawing.Point(17, 49);
+            this.lvAanwezigeBezoekers.Name = "lvAanwezigeBezoekers";
+            this.lvAanwezigeBezoekers.Size = new System.Drawing.Size(660, 306);
+            this.lvAanwezigeBezoekers.TabIndex = 12;
+            this.lvAanwezigeBezoekers.UseCompatibleStateImageBehavior = false;
+            // 
+            // txtBezoekerAanmelden
+            // 
+            this.txtBezoekerAanmelden.Location = new System.Drawing.Point(298, 379);
+            this.txtBezoekerAanmelden.Name = "txtBezoekerAanmelden";
+            this.txtBezoekerAanmelden.Size = new System.Drawing.Size(120, 20);
+            this.txtBezoekerAanmelden.TabIndex = 13;
+            this.txtBezoekerAanmelden.TextChanged += new System.EventHandler(this.txtBezoekerAanmelden_TextChanged);
+            // 
+            // lblBezoekerAanmelden
+            // 
+            this.lblBezoekerAanmelden.AutoSize = true;
+            this.lblBezoekerAanmelden.Location = new System.Drawing.Point(179, 382);
+            this.lblBezoekerAanmelden.Name = "lblBezoekerAanmelden";
+            this.lblBezoekerAanmelden.Size = new System.Drawing.Size(113, 13);
+            this.lblBezoekerAanmelden.TabIndex = 14;
+            this.lblBezoekerAanmelden.Text = "Bezoeker aanmelden: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(438, 382);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Bezoeker afmelden:";
+            // 
+            // txtBezoekerAfmelden
+            // 
+            this.txtBezoekerAfmelden.Location = new System.Drawing.Point(557, 379);
+            this.txtBezoekerAfmelden.Name = "txtBezoekerAfmelden";
+            this.txtBezoekerAfmelden.Size = new System.Drawing.Size(120, 20);
+            this.txtBezoekerAfmelden.TabIndex = 15;
+            this.txtBezoekerAfmelden.TextChanged += new System.EventHandler(this.txtBezoekerAfmelden_TextChanged);
+            // 
+            // btnBetalingsgegevens
+            // 
+            this.btnBetalingsgegevens.Location = new System.Drawing.Point(593, 12);
+            this.btnBetalingsgegevens.Name = "btnBetalingsgegevens";
+            this.btnBetalingsgegevens.Size = new System.Drawing.Size(74, 27);
+            this.btnBetalingsgegevens.TabIndex = 17;
+            this.btnBetalingsgegevens.Text = "Opvragen";
+            this.btnBetalingsgegevens.UseVisualStyleBackColor = true;
+            this.btnBetalingsgegevens.Click += new System.EventHandler(this.btnBetalingsgegevens_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(437, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Betalingsgegevens Opvragen:";
+            // 
+            // FormAanwezigeBezoekers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 414);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBetalingsgegevens);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBezoekerAfmelden);
+            this.Controls.Add(this.lblBezoekerAanmelden);
+            this.Controls.Add(this.txtBezoekerAanmelden);
+            this.Controls.Add(this.lvAanwezigeBezoekers);
+            this.Controls.Add(this.lbSluiten);
+            this.Controls.Add(this.lblAanwezigeBezoekers);
+            this.Name = "FormAanwezigeBezoekers";
+            this.Text = "AanwezigBezoekers";
+            this.Load += new System.EventHandler(this.AanwezigBezoekers_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAanwezigeBezoekers;
+        private System.Windows.Forms.Button lbSluiten;
+        private System.Windows.Forms.ListView lvAanwezigeBezoekers;
+        private System.Windows.Forms.TextBox txtBezoekerAanmelden;
+        private System.Windows.Forms.Label lblBezoekerAanmelden;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBezoekerAfmelden;
+        private System.Windows.Forms.Button btnBetalingsgegevens;
+        private System.Windows.Forms.Label label2;
+    }
+}
